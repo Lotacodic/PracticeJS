db0694aa5622931c725188a0fa221e52d02f5263
Lotacodic <Ngenejoshua05@gmail.com>
2023-03-16 23:34:32 +0100

    Add event listener

--- Dom-and-Events-fundamentals/Guess-my-number/script.js
+++ Dom-and-Events-fundamentals/Guess-my-number/script.js
@@ -12,11 +12,20 @@ console.log(document.querySelector('.guess').value);
 */
 
 //Handling click events
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', () =>{
 const guess = Number(document.querySelector('.guess').value);
 console.log(guess, typeof guess);
 
 if (!guess) {
     document.querySelector(".message").textContent = 'No number!';
+} else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+} else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too high!';
+} else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too low!';
 }
 })
