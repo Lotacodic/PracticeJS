2416fc4c317ba82bb2e242400a835d0ec9f49623
Lotacodic <Ngenejoshua05@gmail.com>
2023-03-18 18:33:47 +0100

    First commit

---
+++ modal/script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const modal = document.querySelector('.modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+const openModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+for( let i = 0; i < btnOpenModal.length; i++) {
+    btnOpenModal[i].addEventListener('click', openModal)
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', (e) => {
+    console.log(e.key);
+
+    if (e.key === 'Escape' && !modal.classList.contains('hidden'))
+    closeModal();
+})
