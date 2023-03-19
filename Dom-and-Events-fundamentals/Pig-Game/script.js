dce533e0a38c00352a2aa2b0c26ff1022b44820e
Lotacodic <Ngenejoshua05@gmail.com>
2023-03-19 22:16:05 +0100

    First commit

---
+++ Dom-and-Events-fundamentals/Pig-Game/script.js
@@ -0,0 +1,27 @@
+'use strict';  
+  
+// Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.getElementById('dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', () => {
+    // Generate a random dice
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice)
+
+    // Display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    // Check for rolled 1, switch to next player
+})
