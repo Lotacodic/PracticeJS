adca2e1a91b43ac0728cf682e8f31e7ae9adf1d5
Lotacodic <Ngenejoshua05@gmail.com>
2023-03-21 17:21:04 +0100

    Change project path

--- Dom-and-Events-fundamentals/Guess-my-number/script.js
+++ Dom-and-Events-fundamentals/Guess-my-number/script.js
@@ -1,15 +1,4 @@
 'use strict';
-/*
-console.log(document.querySelector(".message").textContent);
- 
-document.querySelector('.message').textContent = 'Correct Number!';
-
-document.querySelector('.number').textContent = 14;
-document.querySelector('.score').textContent = 11;
-
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
-*/
 
 //Handling click events
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
