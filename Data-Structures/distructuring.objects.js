e62946f7f6ecb4c4a57867f6ffc732a7a1ff16c6
Lotacodic <Ngenejoshua05@gmail.com>
2023-03-25 20:31:06 +0100

    First commit

---
+++ Data-Structures/distructuring.objects.js
@@ -0,0 +1,69 @@
+"use strict";
+
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Visa Angelo Travanti 23, firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  startMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 12,
+    },
+    fri: {
+      open: 11,
+      close: 22,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.startMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({ starterIndex = 1, mainIndex = 0, time, address }) {
+    console.log(
+      `Order received! ${this.startMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+};
+
+restaurant.orderDelivery({
+  starterIndex: 2,
+  address: "Via del Sole, 21",
+  time: "22:30",
+});
+
+//Note: in objects the order of the elements does not matter
+
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+//Default values
+const { menu = [], startMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+// Mutating variables while destructing objects
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+
+({ a, b } = obj);
+console.log(a, b);
+
+// Nested objects
+const {
+  fri: { open: starts, close: ends },
+} = openingHours;
+console.log(starts, ends);
